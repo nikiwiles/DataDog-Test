5adc8847edda347d30a1acd3a0ec33fe0f05b353
U-EMEA-MEDIA\niki.wiles <niki.wiles@GBLON10DT120.emea.media.global.loc>
2016-07-01 15:55:46 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StatsdClient;
+
+namespace DataDogIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Configire on localhost
+            var DogStatsdConfig = new StatsdConfig
+            {
+                StatsdServerName = "127.0.0.1",
+                StatsdPort = 8125,
+                Prefix = "My-Test-App" 
+            };
+
+            // Apply configuration
+            StatsdClient.DogStatsd.Configure(DogStatsdConfig);
+
+            // Send an event
+            DogStatsd.Increment("MyEvent");
+        }
+    }
+}
